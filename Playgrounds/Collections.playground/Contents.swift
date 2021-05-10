5e7c9e72192ac96e0eb82171932c576e0a09f394
Al Tyus <alctyus@gmail.com>
2021-05-10 10:31:25 -0400

    update collections

--- Playgrounds/Collections.playground/Contents.swift
+++ Playgrounds/Collections.playground/Contents.swift
@@ -33,3 +33,29 @@
  Print the first two musicians to the console.
  */
 	
+//: Dictionaries
+
+/*:
+ Declare a variable `sharePrices` and store into it a dictionary whose keys are stock tickers ex. `AAPL` and whose values are the current stock price ex. 128.46.  Let's add 5 companies and prices.
+ */
+
+/*:
+ Add an additional company and share price to the dictionary.
+ */
+
+/*:
+ Remove a company and share price from the dictionary.
+ */
+
+/*:
+ Use subscript notation to print the value of your favorite company's share price to the console.
+ */
+
+/*:
+ Use a for-in or forEach loop to print the value of each key and value to the dictionary.  Use String Literal notation to create the following string for a ticker of `AAPL` and a value of 128.46 `Ticker: AAPL, Share Price: 128.46`
+ */
+
+/*:
+ Print the tickers of all companies whose share price is greater than or equal to 100
+ */
+
