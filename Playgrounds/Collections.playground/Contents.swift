fac7dbefb13d47feb0a8ac9ccdb5b0969c166708
Al Tyus <alctyus@gmail.com>
2021-05-09 23:13:47 -0400

    add collections playground

---
+++ Playgrounds/Collections.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Arrays
+
+/*:
+ Declare a constant `musicians` and assign to it an array of the names (String) of four of your favorite musicians or groups.
+ */
+
+let musicians = ["Kanye", "Kendrick", "J Cole", "Train"]
+
+/*:
+ Add one more of your favorite musicians to `musicians`.. Whoops, you can't because musicians is a `let` constant. Update `musicians ` so that you can add an additional favorite musician to it.
+ */
+
+/*:
+ Sort musicians in alphabetical order and assign to a new variable `var` `sortedMusicians`.
+ */
+
+/*:
+ Mutate the last musicians array by removing the last musician.
+ */
+
+/*:
+ Print the first two musicians in `sortedMusicians` to the console.
+ */
+
+/*:
+ Print a random musician to the console.
+ */
+
+/*:
+ Print the last two musicians to the console.
+ */
+
+/*:
+ Print the first two musicians to the console.
+ */
